a7cac06ac06fe0347520bea9bbdb002e6e695bf6
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-06-18 14:22:51 +0200

    fully functional chatbot for geoloaction

--- EsTr-langchain-ai-app/src/chat-ai/components/Chat-card.component.tsx
+++ EsTr-langchain-ai-app/src/chat-ai/components/Chat-card.component.tsx
@@ -1,6 +1,13 @@
 import { useRef } from "react";
 import ResponseField from "./Response-field.component"
 
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema"; /* send puur messages */
+import { SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate } from "langchain/prompts";
+
+
+
+
 type ChatCardProps = {
     message: string;
     responseMessage: string[];
@@ -9,6 +16,14 @@ type ChatCardProps = {
     isChatActive: boolean;
     setIsChatActive: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+
+const model = new ChatOpenAI({
+    openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    modelName: 'gpt-3.5-turbo',
+    temperature: 0,
+    verbose: true,
+});
 
 const ChatCard = ({ message, responseMessage, setMessage, setResponseMessage, isChatActive, setIsChatActive }: ChatCardProps) => {
 
@@ -19,10 +34,43 @@ const ChatCard = ({ message, responseMessage, setMessage, setResponseMessage, is
         setMessage(e.target.value);
     };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const simpleCall = async (message: string)=> {
+       const response = model.call([
+            new HumanChatMessage(
+                message
+            ),
+        ]);
+        return response;
+    }
+
+    
+
+    const templateCall = async(message:string) => {
+        const templatePrompt = ChatPromptTemplate.fromPromptMessages([
+            SystemMessagePromptTemplate.fromTemplate(
+              "You are a helpful geographic assistant that helps with places geo locations. If the information is not enough just answer there is not enough information"
+            ),
+            HumanMessagePromptTemplate.fromTemplate(`Extracts the name of the place and return the geographic coordinates of the place from the following phrase: {message}`),
+          ]);
+
+       const response = model.generatePrompt([
+            await templatePrompt.formatPromptValue({
+              message: message,
+            }),
+          ]);
+          return response
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        // const response = await simpleCall(message);
+        const response = await templateCall(message);
         setResponseMessage((prevValue) => [...prevValue, message]);
-        setResponseMessage((prevValue) => [...prevValue, 'some message as response'])
+        // const res = response.generations.map((item)=>item);
+        const res = response.generations[0][0].text;
+        console.log('here:', res)
+        setMessage('');
+        setResponseMessage((prevValue) => [...prevValue, res])
     }
 
     const handleChat = () => {
@@ -32,15 +80,15 @@ const ChatCard = ({ message, responseMessage, setMessage, setResponseMessage, is
     }
 
     return (
-        <div className= {`main-container 
+        <div className={`main-container 
                          py-10 
                          -mt-[45vw] 
                          transition-all 
                          duration-300 
                          ${isChatActive ? 'ml-0]' : 'ml-[101vw]'}`
-                         }>
-            <div data-theme='dark' 
-            className='card lg:card-side card-bordered border-gray-600 shadow-md shadow-stone-600 w-[80vw] py-4 mx-auto mb-6 text-gray-200 items-center'>
+        }>
+            <div data-theme='dark'
+                className='card lg:card-side card-bordered border-gray-600 shadow-md shadow-stone-600 w-[80vw] py-4 mx-auto mb-6 text-gray-200 items-center'>
                 <div className="card-body mx-0 py-2 items-center">
                     <h2 className="card-title text-[.8rem] sm:text-[1.2rem] ">OpenAI & Langchain </h2>
                     <p className='text-[.6rem] sm:text-[.8rem]'>First test web-app</p>
@@ -50,6 +98,7 @@ const ChatCard = ({ message, responseMessage, setMessage, setResponseMessage, is
 
                 <form onSubmit={handleSubmit} className="form-control w-[100%] px-[7%]">
                     <input type="text"
+                        required
                         placeholder="your message"
                         className="input w-[100%] text-sm input-sm border-yellow-500 border-[1px]"
                         value={message}
--- EsTr-langchain-ai-app/src/chat-ai/components/Response-field.component.tsx
+++ EsTr-langchain-ai-app/src/chat-ai/components/Response-field.component.tsx
@@ -15,9 +15,13 @@ const ResponseField = ({ responseMessage }: ResponseProps) => {
     return (
         <div className='w-[100%] ml-[11vw] max-h-[90vh]'>
             {responseMessage.length
-                ? <div className="card w-[86%] max-h-60 sm:max-h-40 overflow-y-scroll lg:card-side card-bordered border-gray-600 my-4 text-gray-200 items-start rounded-md">
+                ? <div className=" card w-[86%] max-h-60 sm:max-h-40 overflow-y-scroll lg:card-side card-bordered border-gray-600 my-4 text-gray-200 items-start rounded-md">
                     <div className="card-body text-sm px-2 py-4">
-                        {responseMessage.map((item, index) => <p key={index}>{item}</p>)}
+                        {responseMessage.map((item, index) => {
+                            const color = index % 2 == 0 ? 'text-lime-400' : 'text-amber-100'
+                            return <p key={index} className={color}>{item}</p>
+                        }
+                        )}
                     </div>
                     <div ref={scrollRef}></div>
                 </div>
