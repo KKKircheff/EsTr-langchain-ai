6cd2fc8cebdf545850da3ec3896a5d2e9622a7c5
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-06-22 22:32:18 +0200

    some crazy tries

---
+++ EsTr-langchain-ai-app/netlify/functions/templateCall.js
@@ -0,0 +1,50 @@
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { LLMChain } from 'langchain/chains';
+import { SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate } from 'langchain/prompts';
+
+export const handler = async (event) => {
+
+  const keyOpenAPI = process.env.VITE_OPENAI_API_KEY;
+
+  const { message } = JSON.parse(event.body);
+
+  const model = new ChatOpenAI({
+    openAIApiKey: keyOpenAPI,
+    modelName: 'gpt-3.5-turbo',
+    temperature: 0,
+    verbose: true
+  });
+
+  const templatePrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      'You are a helpful geographic assistant that helps with geo locations of a place.  If the information is not enough just answer, that there is not enough information'
+    ),
+    HumanMessagePromptTemplate.fromTemplate(
+      `Extracts the name of the place and return the geographic coordinates of the place from the following phrase: {message}`
+    ),
+  ]);
+
+  const chain = new LLMChain({
+    llm: model,
+    prompt: templatePrompt
+  });
+
+  try {
+    const response = await chain.call({
+      message: message
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        response: response
+      })
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        response: 'Sorry something went wrong with this search. Could you be more specific, please.'
+      })
+    }
+  }
+}
--- EsTr-langchain-ai-app/netlify/functions/test.js
+++ EsTr-langchain-ai-app/netlify/functions/test.js
@@ -1,11 +1,10 @@
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { BraveSearch } from 'langchain/tools';
-
 import { Calculator } from 'langchain/tools/calculator';
 import { ChatAgent, AgentExecutor } from 'langchain/agents';
 
 exports.handler = async function(event) {
-
+    
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Hello World"})
