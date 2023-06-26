b4a6e0c263016a5c17cbeaa6388fe071263f8723
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-06-26 18:42:20 +0200

    functions refactored 'onCall'

--- EsTr-langchain-ai-app/functions/src/simpleCall.ts
+++ EsTr-langchain-ai-app/functions/src/simpleCall.ts
@@ -1,53 +1,45 @@
-import { https } from 'firebase-functions';
+import * as functions from 'firebase-functions'
+// import { https } from 'firebase-functions';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { BraveSearch } from 'langchain/tools';
 import { Calculator } from 'langchain/tools/calculator';
 import { initializeAgentExecutorWithOptions } from 'langchain/agents';
 
-const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers':
-    'Origin, X-Requested-With, Content-Type, Accept',
-};
 
-export const handler = https.onRequest(async (req, res) => {
-  const message = req.query.parameter;
-  const keyOpenAPI = process.env.key_openapi;
-  const keyBrave = process.env.key_brave;
+export const handler = functions.https.onCall(async (data, context) => {
+    const { message } = data;
+    const keyOpenAPI = process.env.key_openapi;
+    const keyBrave = process.env.key_brave;
 
-  const model = new ChatOpenAI({
-    openAIApiKey: keyOpenAPI,
-    modelName: 'gpt-3.5-turbo',
-    temperature: 0,
-    verbose: true,
-  });
-
-  const tools = [
-    new BraveSearch({
-      apiKey: keyBrave,
-    }),
-    new Calculator(),
-  ];
-
-  const executor = await initializeAgentExecutorWithOptions(
-    tools,
-    model,
-    {
-      agentType: 'chat-zero-shot-react-description',
-      verbose: true,
-    }
-  );
-
-  const input = message;
-
-  try {
-    const response = await executor.call({ input });
-    res.set(CORS_HEADERS);
-    res.status(200).json({ response });
-  } catch (error) {
-    res.set(CORS_HEADERS);
-    res.status(400).json({
-      response: `Sorry, something went wrong with this search. I tried hard but could not find a proper result. ${error}`,
+    const model = new ChatOpenAI({
+        openAIApiKey: keyOpenAPI,
+        modelName: 'gpt-3.5-turbo',
+        temperature: 0,
+        verbose: true,
     });
-  }
+
+    const tools = [
+        new BraveSearch({
+            apiKey: keyBrave,
+        }),
+        new Calculator(),
+    ];
+
+    const executor = await initializeAgentExecutorWithOptions(
+        tools,
+        model,
+        {
+            agentType: 'chat-zero-shot-react-description',
+            verbose: true,
+        }
+    );
+
+    const input = message;
+
+    try {
+        const response = await executor.call({ input });
+        return response
+    } catch (error) {
+        return `Sorry, something went wrong with this search. I tried hard but could not find a proper result. ${error}`
+    }
 });
--- EsTr-langchain-ai-app/src/main.tsx
+++ EsTr-langchain-ai-app/src/main.tsx
@@ -1,21 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { initializeApp } from 'firebase/app';
 import App from './App.tsx'
 import './index.css'
 
-
-const firebaseConfig = {
-
-    apiKey: "AIzaSyBFDZeBpfqQrkD4KRDS5jvljkgKWJciIhc",
-    authDomain: "langchainai-8e76a.firebaseapp.com",
-    projectId: "langchainai-8e76a",
-    storageBucket: "langchainai-8e76a.appspot.com",
-    messagingSenderId: "550169279166",
-    appId: "1:550169279166:web:97646610c6fa71b463bcef"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 //   <React.StrictMode>
